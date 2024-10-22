6fe76de8153212996093a553f9f454fa98e57c05
Kyle Scharnhorst <kyle.scharnhorst@gmail.com>
2024-10-22 11:51:12 -0700

    Add: copying results to clipboard.

--- background.js
+++ background.js
@@ -1,33 +1,10 @@
 browser.browserAction.onClicked.addListener(async (tab) => {
-    // disable the browser action for the tab
-    // browser.browserAction.disable(tab.id);
-    // requires the "tabs" or "activeTab" permission, or host permissions for the URL
-    //console.log(tab.url);
-    // const links = content.document.querySelectorAll('a');
-
-    // for (const link of links) {
-    //     console.log(link.href); // Prints the URL of each link
-    // }
-
     try {
         await browser.scripting.executeScript({
             target: {
                 tabId: tab.id,
             },
-            func: () => {
-                document.body.style.border = "5px solid green";
-                // const links = document.querySelectorAll('a');
-
-                // for (const link of links) {
-                //     console.log(link.href); // Prints the URL of each link
-                // }
-
-                // for (const link in document.links) {
-                //     console.log(link.href);
-                // }
-                // https://www.faceit.com/en/cs2/room/1-303bcfc7-ff80-4028-bf06-69f1adebd748
-                // en\/cs2\/room\/[0-9a-z]{1}-[0-9a-z]{8}-[0-9a-z]{4}-[0-9a-z]{4}-[0-9a-z]{4}-[0-9a-z]{12}
-
+            func: async () => {
                 var links = document.getElementsByTagName('a');
                 var match_ids = [];
                 for (var i = 0; i < links.length; i++) {
@@ -42,8 +19,14 @@ browser.browserAction.onClicked.addListener(async (tab) => {
                         }
                     }
                 }
-
-                console.log('Match ids: ' + JSON.stringify(match_ids));
+                
+                var match_ids_json = JSON.stringify(match_ids);
+                console.log('Match ids: ' + match_ids_json);
+                try {
+                    await navigator.clipboard.writeText(match_ids_json);
+                  } catch (error) {
+                    console.error(error.message);
+                }
             },
         });
     } catch (err) {
